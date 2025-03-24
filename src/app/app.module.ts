ebbf63d3830e6f01910c6d554b11892dbbf416d2
shivali <shivalipradhan1010@gmail.com>
2025-03-24 23:52:58 +0530

    fixed import form error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 // src/app/app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    
-    
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
