f4ce098c70b19e783762ed356aa69c0a2b77a13e
shivali <shivalipradhan1010@gmail.com>
2025-03-24 23:25:11 +0530

    final design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formflow';
+  formBuilder: FormGroup;
+  questions: FormArray;
+
+  constructor(private fb: FormBuilder) {
+    this.formBuilder = this.fb.group({
+      title: [''],
+      description: [''],
+      questions: this.fb.array([]),
+    });
+    this.questions = this.formBuilder.get('questions') as FormArray;
+  }
+
+  addQuestion() {
+    this.questions.push(this.fb.group({
+      questionText: ['Question text'],
+      type: ['shortText'],
+      options: this.fb.array([]),
+      required: false, 
+    }));    
+  }
+
+  duplicateQuestion(index: number) {
+    const originalQuestion = this.questions.at(index).value; 
+    const duplicatedQuestion = this.fb.group({
+      questionText: [originalQuestion.questionText],
+      type: [originalQuestion.type],
+      required: [originalQuestion.required],
+      options: this.fb.array(
+        originalQuestion.options ? originalQuestion.options.map((opt: any) => this.fb.control(opt)) : []
+      )
+    });
+    this.questions.insert(index + 1, duplicatedQuestion); 
+    
+  }
+  removeQuestion(index: number) {
+    this.questions.removeAt(index);
+  }
+
+  addOption(questionIndex: number) {
+    const question = this.questions.at(questionIndex) as FormGroup;
+    let options = question.get('options') as FormArray;
+
+    if (!options) {
+      options = this.fb.array([]);
+      question.setControl('options', options);
+    }
+
+    options.push(new FormControl(''));
+  }
+
+  removeOption(questionIndex: number, optionIndex: number) {
+    const options = this.getOptions(this.questions.at(questionIndex));
+    options.removeAt(optionIndex);
+  }
+
+  getOptions(question: any): FormArray {
+    return question.get('options') as FormArray;
+  }
+
+  onSubmit() {
+    console.log("Form saved");
+    console.log(this.formBuilder.value);
+    // console.log('Form before saving:', JSON.stringify(this.formBuilder.value, null, 2));
+    // this.formService.addForm(this.formBuilder.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,33 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-
 import { AppComponent } from './app.component';
-import { FormBuilderComponent } from './form-builder/form-builder.component';
-import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FormBuilderComponent,
-    QuestionComponent,
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatSelectModule,
-    MatCheckboxModule,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
+    
     
   ],
   providers: [],
